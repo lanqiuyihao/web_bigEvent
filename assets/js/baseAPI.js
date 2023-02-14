ba094a89eb89efbcc33ab0b7bc70e948b8e0f752
php粉丝控 <lanqiuyihao@126.com>
2023-02-14 21:17:51 +0800

    完成了后台首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 // 每次发起Ajax请求之前拼接url
 $.ajaxPrefilter(function(options){
     options.url = 'http://www.liulongbin.top:3007'+options.url
+
+    // 为有权限的接口统一配置 header头
+    if(options.url.indexOf('/my/')!==-1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 为后台有权限页面设置权限访问
+    options.complete = function(res){
+        if(res.responseJSON.status===1 && res.responseJSON.message.indexOf('失败')!==-1){
+            
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function(){
+    getUserInfo()
+
+    function getUserInfo(){
+        $.ajax({
+            method:'GET',
+            url:'/my/userinfo',
+            // headers:{
+            //     Authorization:localStorage.getItem('token')
+            // },
+            success:function(res){
+                if(res.status===0){
+                    renderAvatar(res.data)
+                }
+            }
+        })
+    }
+    function renderAvatar(user){
+        var name = user.nickname || user.username
+        $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+        if(user.user_pic!==null){
+            $('.layui-nav-img').attr('src',user.user_pic).show()
+            $('.avatar_text').hide()
+        }else{
+            $('.layui-nav-img').hide()
+            var first =  name[0].toUpperCase()
+            $('.avatar_text').css('display','inline-block').html(first).show()
+        }
+    }
+
+
+    var layer = layui.layer
+    // 退出逻辑
+    $('#btnLogout').on('click',function(){
+        
+        layer.confirm('确定退出吗？',{icon:3,title:'提示'},function(index){
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index)
+        })
+    })
+})
+
